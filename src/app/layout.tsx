0b64f8c6d18b4614456fc07a8acb52d855e29165
Sergey <sergeypavlov@redcollar.ru>
2025-04-22 16:52:43 +0300

    update head scripts for app routes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
-import { Partytown } from "@qwik.dev/partytown/react";
+// import { Partytown } from "@qwik.dev/partytown/react";
+import Script from "next/script";
 // import Script from "next/script";
 
 export default function RootLayout({
@@ -9,36 +10,73 @@ export default function RootLayout({
   return (
     <html lang="en">
     <head>
-      <Partytown forward={["dataLayer.push", "ym", "ym.a", "ym.a.push"]}
-                 lib="/nextjs-partytown-ym/~partytown/"
+      <Script
+        src="/nextjs-partytown-ym/~partytown/partytown.js"
+        strategy="beforeInteractive"
       />
       <script
-        id="ym"
-        type="text/partytown"
-        // strategy="worker"
-        suppressHydrationWarning
         dangerouslySetInnerHTML={{
           __html: `
-                      window.dataLayer = window.dataLayer || [];
-                      (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-                        m[i].l=1*new Date();
-                        for (var j = 0; j < document.scripts.length; j++) {
-                          if (document.scripts[j].src === r) { return; }
-                        }
-                        k=e.createElement(t),
-                        a=e.getElementsByTagName(t)[0],
-                        k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
-                      })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-        
-                      ym(101262698, "init", {
-                        clickmap:true,
-                        trackLinks:true,
-                        accurateTrackBounce:true,
-                        webvisor:true
-                      });
-                    `,
+              partytown = {
+                forward: ["dataLayer.push", "ym", "ym.a", "ym.a.push"]
+              };
+            `,
         }}
       />
+      <script
+        type="text/partytown"
+        dangerouslySetInnerHTML={{
+          __html: `
+              window.dataLayer = window.dataLayer || [];
+              (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                m[i].l=1*new Date();
+                for (var j = 0; j < document.scripts.length; j++) {
+                  if (document.scripts[j].src === r) { return; }
+                }
+                k=e.createElement(t),
+                a=e.getElementsByTagName(t)[0],
+                k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+              })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+              ym(101262698, "init", {
+                clickmap:true,
+                trackLinks:true,
+                accurateTrackBounce:true,
+                webvisor:true
+              });
+            `,
+        }}
+      />
+      {/*<Partytown forward={["dataLayer.push", "ym", "ym.a", "ym.a.push"]}*/}
+      {/*           // lib="/nextjs-partytown-ym/~partytown/"*/}
+      {/*/>*/}
+      {/*<script*/}
+      {/*  id="ym"*/}
+      {/*  type="text/partytown"*/}
+      {/*  // strategy="worker"*/}
+      {/*  // suppressHydrationWarning*/}
+      {/*  dangerouslySetInnerHTML={{*/}
+      {/*    __html: `*/}
+      {/*                window.dataLayer = window.dataLayer || [];*/}
+      {/*                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};*/}
+      {/*                  m[i].l=1*new Date();*/}
+      {/*                  for (var j = 0; j < document.scripts.length; j++) {*/}
+      {/*                    if (document.scripts[j].src === r) { return; }*/}
+      {/*                  }*/}
+      {/*                  k=e.createElement(t),*/}
+      {/*                  a=e.getElementsByTagName(t)[0],*/}
+      {/*                  k.async=1,k.src=r,a.parentNode.insertBefore(k,a)*/}
+      {/*                })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");*/}
+      {/*  */}
+      {/*                ym(101262698, "init", {*/}
+      {/*                  clickmap:true,*/}
+      {/*                  trackLinks:true,*/}
+      {/*                  accurateTrackBounce:true,*/}
+      {/*                  webvisor:true*/}
+      {/*                });*/}
+      {/*              `,*/}
+      {/*  }}*/}
+      {/*/>*/}
     </head>
     <body>
     {children}
